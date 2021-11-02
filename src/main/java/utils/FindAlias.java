92dfa79924de2c6fa0a021db010193881e905c77
Otávio Cury <otaviocury.oc@gmail.com>
2021-11-02 20:59:36 -0300

    Adding FindAlias method

--- src/main/java/utils/FindAlias.java
+++ src/main/java/utils/FindAlias.java
@@ -1,5 +1,32 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import dao.ContributorDAO;
+import model.Contributor;
+
 public class FindAlias {
+	
+	public static List<String> emails(Contributor contributor){
+		List<String> emails = new ArrayList<String>();
+		ContributorDAO contributorDAO = new ContributorDAO();
+		List<Contributor> contributors = contributorDAO.findAll(Contributor.class);
+		for(Contributor contributorAux: contributors) {
+			if(contributorAux.getEmail().equals(contributor.getEmail()) == false) {
+				String nome = contributorAux.getName();
+				if(nome != null) {
+					int distancia = StringUtils.getLevenshteinDistance(contributor.getName(), nome);
+					if (nome.equals(contributor.getName()) || 
+							(distancia/(double)contributor.getName().length() < 0.3)) {
+						emails.add(contributorAux.getEmail());
+					}
+				}
+			}
+		}
+		return emails;
+	}
 
 }
